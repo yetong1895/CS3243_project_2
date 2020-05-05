8c011fe17f2858ffb78ad011d2569a1003b92fb9
mrytg <e0310555@u.nus.edu>
2020-05-05 11:38:13 +0800

    test

--- experiment.py
+++ experiment.py
@@ -28,8 +28,8 @@ MRV_timing = []
 
 # read inputs
 
-for filename in os.listdir(os.getcwd() + "/testcases/"):
-	with open(os.path.join(os.getcwd() + "/testcases/", filename), 'r') as f:
+for filename in os.listdir(os.getcwd() + "/public_tests_p2_sudoku/"):
+	with open(os.path.join(os.getcwd() + "/public_tests_p2_sudoku/", filename), 'r') as f:
 		# Taken from main
 		puzzle = readPuzzle(f)
 		# run each algorithm for 5 times and take average
